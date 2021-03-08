e6831341d60f4bc5a101dc06b6da2f38717178df
Rina Zelmann <rzelmann@mgh.harvard.edu>
2021-03-08 13:18:19 -0500

    changed bayes classifier to follow Navid's code

--- Models/CLEAR/Model/nz_bayes_classifier_CLoSES.m
+++ Models/CLEAR/Model/nz_bayes_classifier_CLoSES.m
@@ -8,10 +8,11 @@ function [z,t]=nz_bayes_classifier_CLoSES(m,S,X)
 %[N,unused]=size(X); % N=no. of vectors
 t = zeros(1,c); % RIZ: CAN I FIX it instead of size? -  before: [];
 %z = zeros(1,N); 
-constant1 = 1;
+%constant1 = 1;
 %for i=1:N % RIZ removed as only 1 feature is considered each time
     for j=1:c
-        t(j)=constant1*exp(-0.5*(X-m(j,:))*inv(squeeze(S(j,:,:)))*(X-m(j,:))');
+        t(j)=mvnpdf(X,m(j,:),squeeze(S(j,:,:)));
+       % t(j)=constant1*exp(-0.5*(X-m(j,:))*inv(squeeze(S(j,:,:)))*(X-m(j,:))');
      %   t(i,j)=constant1*exp(-0.5*(X(i,:)-m(j,:))*inv(reshape(S(j,:,:),[l,l]))*(X(i,:)-m(j,:))');
     end
     % Determining the maximum quantity Pi*p(x|wi)
