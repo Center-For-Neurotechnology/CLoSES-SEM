12d8266a643d68336825a183195eacfcd27bd503
Navid Ziaei <35403134+Navid-Ziaei@users.noreply.github.com>
2021-03-13 14:53:39 +0330

    change nz_bayes_classifier_CLoSES

--- Models/CLEAR/Model/nz_bayes_classifier_CLoSES.m
+++ Models/CLEAR/Model/nz_bayes_classifier_CLoSES.m
@@ -11,7 +11,8 @@ t = zeros(1,c); % RIZ: CAN I FIX it instead of size? -  before: [];
 constant1 = 1;
 %for i=1:N % RIZ removed as only 1 feature is considered each time
     for j=1:c
-        t(j)=constant1*exp(-0.5*(X-m(j,:))*inv(squeeze(S(j,:,:)))*(X-m(j,:))');
+		t(j) = (2*pi)^(-length(m(j,:))/2)*det(squeeze(S(j,:,:)))^(-1/2)*exp(-0.5*(X-m(j,:))*inv(squeeze(S(j,:,:)))*(X(-m(j,:))');
+        %t(j)=constant1*exp(-0.5*(X-m(j,:))*inv(squeeze(S(j,:,:)))*(X-m(j,:))');
      %   t(i,j)=constant1*exp(-0.5*(X(i,:)-m(j,:))*inv(reshape(S(j,:,:),[l,l]))*(X(i,:)-m(j,:))');
     end
     % Determining the maximum quantity Pi*p(x|wi)
